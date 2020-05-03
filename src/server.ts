2dcbb106fc977ff23c45cd6abb4cc63ed869d9a7
Yudi Yamane <yudiyamane@gmail.com>
2020-05-03 12:13:44 -0300

    added global error handling

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import routers from './routers';
 
+import AppError from './errors/AppError';
+
 const app = express();
 
+app.use(express.json());
 app.use(routers);
 
-app.get('/', (request, response) => response.send('ok'));
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+      status: 'error',
+      error: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3000, () => {
-  console.log('Listening');
+  console.log('\n\n\nListening');
 });
