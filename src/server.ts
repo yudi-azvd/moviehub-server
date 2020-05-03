bfafade4eff3ef5d70022c641bfd15f293bff8b4
Yudi Yamane <yudiyamane@gmail.com>
2020-05-03 13:59:43 -0300

    added cors to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routers from './routers';
@@ -8,6 +9,7 @@ import AppError from './errors/AppError';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routers);
 
 app.use(
