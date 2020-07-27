9827040c02aa3dab4a14a84735cdb692c8b74b39
Yudi Yamane <yudiyamane@gmail.com>
2020-07-27 19:03:32 -0300

    details

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 export interface APIMovie {
   id: number;
 
@@ -7,9 +8,13 @@ export interface APIMovie {
 
   overview: string;
 
+  tagline: string;
+
   poster_path: string;
 
   backdrop_path: string;
+
+  vote_average: number;
 }
 
 class Movie {
@@ -21,9 +26,15 @@ class Movie {
 
   overview: string;
 
+  tagline: string;
+
   posterPath: string;
 
   backdropPath: string;
+
+  voteAverage: number;
+
+  static tmdbBaseImageUrl = process.env.TMDB_BASE_IMAGE_URL;
 
   constructor({
     title,
@@ -32,13 +43,17 @@ class Movie {
     id,
     poster_path,
     backdrop_path,
+    vote_average,
+    tagline,
   }: APIMovie) {
     this.id = id;
     this.title = title;
     this.originalTitle = original_title;
     this.overview = overview;
-    this.posterPath = poster_path;
-    this.backdropPath = backdrop_path;
+    this.tagline = tagline;
+    this.voteAverage = vote_average;
+    this.posterPath = `${Movie.tmdbBaseImageUrl}/w300${poster_path}`;
+    this.backdropPath = `${Movie.tmdbBaseImageUrl}/w1920_and_h800_multi_faces${backdrop_path}`;
   }
 }
 
